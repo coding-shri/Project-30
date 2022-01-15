0f32a85c45cb21f4531fa17af90fdfed92b86b6e
coding-shri <71526827+coding-shri@users.noreply.github.com>
2022-01-15 18:24:11 -0500

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,38 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 1
+        }
+       this.sling = Constraint.create(options);
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    attach(body){
+        this.sling.bodyA = body;
+    }
+
+    display(){
+        
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            
+            strokeWeight(4);
+            stroke("turquoise")
+            line(pointA.x, PointA.y, PointB.x, PointB.y);
+              
+            }
+           
+            
+        }    
+    }
+    
